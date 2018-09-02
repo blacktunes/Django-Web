9a8189c7ba0595a41687abb436583517729f5acf
Your Name <1719513828@qq.com>
2018-09-02 16:42:24 +0800

    clear files

--- introduce/urls.py
+++ introduce/urls.py
@@ -5,10 +5,10 @@ from . import views
 app_name = 'introduce'
 
 urlpatterns = [
-    url(r'^updata_log/$', views.update_log, name='update_log'),
+    url(r'^update_log/$', views.update_log, name='update_log'),
     url(r'^about/$', views.about, name='about'),
     url(r'^tip/$', views.tip, name='tip'),
     url(r'^about/api/$', views.about_api, name='about_api'),
     url(r'^gamename/api$', views.gamename_api, name='gamename_api'),
-    url(r'^updata_log/api$', views.update_log_api, name='update_log_api'),
+    url(r'^update_log/api$', views.update_log_api, name='update_log_api'),
 ]
